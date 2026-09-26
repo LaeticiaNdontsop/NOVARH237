73184893dbfa8a905b75586d7b8732789cc282e5
Laeticia Ndontsop <ndontsoplaeticia@icloud.com>
2026-09-26 18:14:38 +0100

    Prepare Django settings for Vercel

--- nova_rh/settings.py
+++ nova_rh/settings.py
@@ -115,11 +115,11 @@ USE_TZ = True
 # ---------------------------------------------------------------------------
 # Fichiers statiques et medias (documents RH uploades)
 # ---------------------------------------------------------------------------
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
